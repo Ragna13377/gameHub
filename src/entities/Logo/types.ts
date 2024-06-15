494807b68e7d2948e028c06c353d4e51166914f1
Ivan Dmitrievich <quentin.gothic@gmail.com>
2024-06-15 20:17:56 +0300

    feat: default styles and logo

---
+++ src/entities/Logo/types.ts
@@ -0,0 +1,7 @@
+export type LogoProps = {
+	img: string;
+	width: number;
+	height: number;
+	text?: string;
+	externalStyle?: string;
+};
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,18 @@
-import React from 'react';
+import { useState } from 'react';
+import Logo from '@entities/Logo';
+import logo from '@images/logo.webp';
+import styles from './style.module.scss';
 
-const Index = () => <div>Home</div>;
+const Index = () => {
+	const [appState, setAppState] = useState(false);
+	return (
+		<>
+			<header className={styles.header}>
+				<Logo img={logo} width={90} height={85} text='GameHub' />
+			</header>
+      <main>Текст</main>
+		</>
+	);
+};
 
 export default Index;
